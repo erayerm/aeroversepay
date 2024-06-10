08514ea10f96d68678490fe1acfcd3cd6bfd11e6
erayerm <73521213+erayerm@users.noreply.github.com>
2024-06-10 21:14:28 +0300

    faq content change and fix a problem

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -7,27 +7,27 @@ const questions = [
     },
 
     {
-        question: "How does Aeroverse Pay simplify accepting cryptocurrency for my business?",
+        question: "What security measures does the Trust Screen and Protect Certificate offer?",
         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 
     {
-        question: "How does Aeroverse Pay simplify accepting cryptocurrency for my business?",
+        question: "How can I integrate Aeroverse Pay into my existing payment systems?",
         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 
     {
-        question: "How does Aeroverse Pay simplify accepting cryptocurrency for my business?",
+        question: "What happens once a customer pays with cryptocurrency?",
         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 
     {
-        question: "How does Aeroverse Pay simplify accepting cryptocurrency for my business?",
+        question: "Are there any hidden fees when using Aeroverse Pay?",
         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 
     {
-        question: "How does Aeroverse Pay simplify accepting cryptocurrency for my business?",
+        question: "How does Aeroverse Pay help with compliance and legal issues?",
         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 ]
--- src/components/QuestionAnswer.jsx
+++ src/components/QuestionAnswer.jsx
@@ -8,15 +8,15 @@ export default function QuestionAnswer({ content }) {
     }
     return (
         <div onClick={toggleOpen} className="bg-[#1A1E27] px-[15px] w-[632px] mx-auto">
-            <div className="flex py-[8px] items-center gap-5 ">
-                <p className="font-normal text-[40px]">{isOpen ? "-" : "+"}</p>
-                <p className="font-bold text-[15px]">{content.question}</p>
+            <div className="flex py-[8px] gap-5 ">
+                <div className="w-6"><p className="font-normal text-[40px] w-full">{isOpen ? "-" : "+"}</p></div>
+                <div className="flex flex-col self-center">
+                    <p className="font-bold text-[15px] py-[8px]">{content.question}</p>
+                    {isOpen &&
+                        <p className="font-normal text-[15px]">{content.answer}</p>
+                    }
+                </div>
             </div>
-
-            {isOpen &&
-                <div className="flex py-[8px] gap-2">
-                    <p className="font-normal text-[15px]">{content.answer}</p>
-                </div>}
         </div>
     )
 }
